2b5602d61f24e0f5033c25b1aa5fd764dcf77d52
Huei Tan <huei90@users.noreply.github.com>
2016-01-08 12:37:00 +0800

    Create template-oop.js

---
+++ Template/template-oop.js
@@ -0,0 +1,27 @@
+
+/*
+ * This is the sdk template introduced in javascript-sdk-design
+ *
+ * Template Init
+ * ==============
+ * This is a template that offer oop Object
+ *
+ * To find out more sdk template, please visit javascript-sdk-design homepage
+ * https://github.com/huei90/javascript-sdk-design
+ */
+(function (window) {
+
+    // declare
+    var myApp = function () {
+        return;
+    };
+
+    // your sdk init function
+    myApp.prototype.init = function () {
+        // ...
+    };
+
+    // define your namespace myApp
+    window.myApp = new myApp();
+
+})(window, undefined);
